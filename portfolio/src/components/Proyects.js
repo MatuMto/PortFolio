87ab32a8f0ce548c7b468e6ed516f0b3e5692459
Mateo <matu091201@gmail.com>
2021-07-05 18:17:53 -0300

    modificando responsive de video

--- portfolio/src/components/Proyects.js
+++ portfolio/src/components/Proyects.js
@@ -99,7 +99,10 @@ const Proyects = () => {
                 <div id={index} className="proyectContainer">
                     <div className="carrouselContainer">
                         <Carousel showThumbs={false} className="carrousel" infiniteLoop={true} autoPlay={true} emulateTouch={true}>
-                            {proyect.imgs.map(img => <a href={proyect.hostLink} target="_blank"><div className="carrusel-image" style={{backgroundImage: `url(${img})`}}></div></a>)}
+                            {proyect.imgs.map(img => proyect.hostLink !== '#' ?
+                                 <a href={proyect.hostLink} target="_blank"><div className="carrusel-image" style={{backgroundImage: `url(${img})`}}></div></a>
+                                 : <div onClick={()=>viewMobileProyect(proyect.youtubeLink)} className="carrusel-image" style={{backgroundImage: `url(${img})`}}></div>
+                            )}
                         </Carousel>
                         <div className="projectButton-container">
                             <div onMouseOver={()=> setHoveredButton('proyect' + index)}  onMouseLeave={() => setHoveredButton('noOne')} className="projectButton">
